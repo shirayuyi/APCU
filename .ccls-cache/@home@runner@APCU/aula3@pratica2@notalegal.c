65e1fad47be6b9d301967de49faeeb20678a0ec9
shubakirky <32781488-shubakirky@users.noreply.replit.com>
2024-03-13 13:33:18 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@APCU/aula3@pratica2@notalegal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+  
+printf("---------------------------\n");
+printf("      N O T A  L E G A L     \n");
+printf("---------------------------\n");
+printf("ITEM              QTD      VALOR\n");
+  printf("%14s %i %10.2f\n","Fita isolante 20M ", 3, 20.00);  
+  printf("%14s %i %10.2f\n", "Fita crepe 10M    ", 5, 18.00);
+
+  //printf("Fita isolante 20M      3      20.00\n");
+  //printf("Fita crepe 10M      5      18.00\n");
+
+  printf("---------------------------\n");
+  printf("TOTAL..........: %10.2f\n", 38.00);
+  
+
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@APCU/pratica2@questao07.c
@@ -0,0 +1,39 @@
+/*
+Faça um programa em C que leia o valor da hora de trabalho e o total de horas trabalhadas no mês e calcule o salário bruto, o salário líquido, e os impostos descontados. Considere IR igual a 25% e INSS igual a 11%.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  float valor_da_hora_de_trabalho = 150.0f;
+  float total_de_horas_trabalhadas_no_mes = 100.0f;
+  float valor_salario_bruto = valor_da_hora_de_trabalho * total_de_horas_trabalhadas_no_mes;
+  
+  const float IR = 0.25f;
+  const float INSS = 0.11f;
+
+  float valor_imposto_renda = valor_salario_bruto * IR;
+  float valor_imposto_inss = valor_salario_bruto * INSS;
+
+  float valor_salario_liquido = valor_salario_bruto - valor_imposto_renda - valor_imposto_inss;
+
+  printf("------------------------------------\n");
+  printf("    C O N T R A C H E Q U E   \n");
+  printf("------------------------------------\n");
+  printf("Salario Bruto...: %14.2f\n", valor_salario_bruto);
+  printf("Imposto de Renda...: %14.2f\n", valor_imposto_renda);
+  printf("Imposto INSS...: %14.2f\n", valor_imposto_inss);
+  printf("Salario Liquido...: %14.2f\n", valor_salario_liquido);
+
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
